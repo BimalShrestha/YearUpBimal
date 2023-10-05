977193e255f643400c6925a772e1cd5e20e683a2
BimalShrestha <bimalshrestha1821@gmail.com>
2023-10-05 08:23:40 -0500

    payroll

--- PayrollCalculator/src/main/java/org/example/Main.java
+++ PayrollCalculator/src/main/java/org/example/Main.java
@@ -3,7 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out
+        System.out.println("Enter your name: ");
+        String fullName = scanner.nextLine();
+        System.out.println("Enter your hours worked: ");
+        double hoursWorked = scanner.nextDouble();
+        System.out.println("Enter your pay Rate: ");
+        double payRate = scanner.nextDouble();
+        double grossPay = hoursWorked * payRate;
+        System.out.printf("%s worked %.2f hours with %.2f pay rate and earned %.2f gross pay", fullName,hoursWorked,payRate,grossPay);
 
     }
 }
